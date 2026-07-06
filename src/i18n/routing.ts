e579fd4e649cc02b8c4d3a365fef47458c029329
qw1840317740 <qw1840317740@users.noreply.github.com>
2026-07-06 09:35:07 +0900

    Set English as default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   locales: ["zh", "en", "ja"],
-  defaultLocale: "zh",
+  defaultLocale: "en",
 });
 
 export const { Link, redirect, usePathname, useRouter } =
